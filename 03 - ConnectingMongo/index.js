529434776dd5d8fea42b65b11f5696f9fba3e1c1
LucaFreire <lucasrodifreire@gmail.com>
2023-08-21 14:55:05 -0300

    ToFix: authController

--- 03 - ConnectingMongo/index.js
+++ 03 - ConnectingMongo/index.js
@@ -6,6 +6,7 @@ require('./startup/routes')(app);
 
 const port = 8080;
 routes(app)
-const server = app.listen(port, () => console.log(`server is running http://localhost:${port}`))
+const server = app.listen(port, () => 
+    console.log(`server is running http://localhost:${port}`))
 
 module.exports = server;
