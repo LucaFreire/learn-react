097241c8ba96bb77967f3276beae8f5809c03e39
LucaFreire <lucasrodifreire@gmail.com>
2023-08-22 08:43:38 -0300

    fixed: 02 and 03

--- 02 - NodeAPI/index.js
+++ 02 - NodeAPI/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const routes = require('./routes')
-
 require('./startup/db')();
 require('./startup/routes')(app);
 const port = 5000;
-routes(app)
+
 
 const server = app.listen(port, () => 
     console.log(`🔥server is running!🔥\nhttp://localhost:${port}`))
--- 03 - ConnectingMongo/index.js
+++ 03 - ConnectingMongo/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const routes = require('./routes')
 require('./startup/db')();
 require('./startup/routes')(app);
 
 const port = 8080;
-routes(app)
 const server = app.listen(port, () => console.log(`server is running http://localhost:${port}`))
 
 module.exports = server;
