0738a5cf3a98d2250aebf0f4b990519cbc38039d
LucaFreire <lucasrodifreire@gmail.com>
2023-08-22 08:55:02 -0300

    fixed: 03.1 - Sport

--- 03.1 - Sport/index.mjs
+++ 03.1 - Sport/index.mjs
@@ -1,11 +1,11 @@
 import express from "express"
 import "./loadEnv.mjs"
 import sports from "./controllers/sportsController.mjs"
-import "./startup/db.mjs"
+import connect from "./startup/db.mjs"
 
 const PORT = process.env.PORT || 3000
 const app = express()
-
+const bd = connect()
 app.use(express.json())
 
 app.use("/sports", sports)
